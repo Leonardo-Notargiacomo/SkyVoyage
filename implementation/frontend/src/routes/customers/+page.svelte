8433395c3e6b7713f55d2b55d3540e8c5f9e0561
Leonardo-Notargiacomo <l.notargiacomo@student.fontys.nl>
2025-03-12 11:55:35 +0100

    Update +page.svelte
    Took 6 minutes

--- implementation/frontend/src/routes/customers/+page.svelte
+++ implementation/frontend/src/routes/customers/+page.svelte
@@ -137,7 +137,7 @@
                     </div>
                     <div class="col-span-2">
                         <label for="lastName" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last name</label>
-                        <input type="text" name="lastName" id="lastName" placeholder="First name" required="" bind:value={ newCustomer.lastName } class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                        <input type="text" name="lastName" id="lastName" placeholder="Last name" required="" bind:value={ newCustomer.lastName } class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
                     </div>
                     <div class="col-span-2 sm:col-span-1">
                         <label for="dateOfBirth" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Date of birth</label>
