049ebb1a4a1f1923fe7a734f7924fc2df37aaa07
Furkan Öztürk <furkan1berat@gmail.com>
2025-04-14 10:33:48 +0200

    removed hardcoded url
    system can use dynamic urls now instead of hardcoded one

--- implementation/backend/restapi/src/main/java/io/github/fontysvenlo/ais/restapi/AuthController.java
+++ implementation/backend/restapi/src/main/java/io/github/fontysvenlo/ais/restapi/AuthController.java
@@ -1,15 +1,20 @@
 package io.github.fontysvenlo.ais.restapi;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import io.github.fontysvenlo.ais.businesslogic.api.LoginService;
 import io.github.fontysvenlo.ais.datarecords.LoginRequest;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
-@CrossOrigin(origins = "http://localhost:5173") // adjust for your frontend port
+@CrossOrigin(origins = "*:5173") // Allow any host using port 5173
 public class AuthController {
+
     private final LoginService loginService;
 
     public AuthController(LoginService loginService) {
