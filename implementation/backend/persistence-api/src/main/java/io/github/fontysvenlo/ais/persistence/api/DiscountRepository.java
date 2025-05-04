451ee3e7dd9f55cc2096a0ac1acb94c17f095aba
Leonardo-Notargiacomo <l.notargiacomo@student.fontys.nl>
2025-05-04 20:37:44 +0200

    interface for the discountRepository

---
+++ implementation/backend/persistence-api/src/main/java/io/github/fontysvenlo/ais/persistence/api/DiscountRepository.java
@@ -0,0 +1,20 @@
+package io.github.fontysvenlo.ais.persistence.api;
+
+import io.github.fontysvenlo.ais.datarecords.DiscountData;
+
+import java.util.List;
+
+public interface DiscountRepository {
+
+    DiscountData add(DiscountData discountData);
+
+    DiscountData update(DiscountData discountData);
+
+    DiscountData delete(Integer id);
+
+    DiscountData getOne(Integer id);
+
+    List<DiscountData> getAll();
+
+    List<DiscountData> getByType(String type);
+}
