f8250a29568f438ca38872b942950cbd79b9bb8b
Til Mozes <566541@student.fontys.nl>
2025-05-06 13:22:52 +0200

    Update FlightStatsRepository.java

--- implementation/backend/persistence-api/src/main/java/io/github/fontysvenlo/ais/persistence/api/FlightStatsRepository.java
+++ implementation/backend/persistence-api/src/main/java/io/github/fontysvenlo/ais/persistence/api/FlightStatsRepository.java
@@ -3,6 +3,5 @@ package io.github.fontysvenlo.ais.persistence.api;
 import io.github.fontysvenlo.ais.datarecords.FlightStats;
 
 public interface FlightStatsRepository {
-    // Define the operations you need, e.g.
     FlightStats getStatsForFlight();
 }
