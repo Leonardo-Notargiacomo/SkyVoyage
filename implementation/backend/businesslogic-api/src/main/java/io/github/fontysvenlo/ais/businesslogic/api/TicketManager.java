eff38d81ac289a2f97506af7efeade84486d53fc
ThomasLindelauf <150127275+ThomasLindelauf@users.noreply.github.com>
2025-05-16 14:51:28 +0200

    Removed unused parameter which was forgotten when updating the file earlier

--- implementation/backend/businesslogic-api/src/main/java/io/github/fontysvenlo/ais/businesslogic/api/TicketManager.java
+++ implementation/backend/businesslogic-api/src/main/java/io/github/fontysvenlo/ais/businesslogic/api/TicketManager.java
@@ -9,11 +9,8 @@ import io.github.fontysvenlo.ais.datarecords.TicketData;
 public interface TicketManager {
 
     /**
-     * Retrieves ticket data as a list of lists. Each inner list has 20 indexes,
-     * which can contain empty strings or null values.
-     *
-     * @param id the identifier for the ticket data
-     * @return an ArrayList of ArrayLists, each with 20 indexes
+     * List all tickets
+     * @return a list of TicketData objects
      */
     public List<TicketData> list();
     
