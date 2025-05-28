42c8e0c62176420e26bc8c299c6f11d87022d4df
Leonardo-Notargiacomo <l.notargiacomo@student.fontys.nl>
2025-05-28 09:17:09 +0200

    delete the methods not used in discounts

--- implementation/backend/businesslogic-api/src/main/java/io/github/fontysvenlo/ais/businesslogic/api/DiscountManager.java
+++ implementation/backend/businesslogic-api/src/main/java/io/github/fontysvenlo/ais/businesslogic/api/DiscountManager.java
@@ -1,22 +1,14 @@
 package io.github.fontysvenlo.ais.businesslogic.api;
 
-import io.github.fontysvenlo.ais.datarecords.DiscountData;
 import java.util.List;
-import java.util.Optional;
+
+import io.github.fontysvenlo.ais.datarecords.DiscountData;
 
 public interface DiscountManager {
 
     void addDiscount(DiscountData discountData);
 
-    void updateDiscount(DiscountData discountData);
-
     void deleteDiscount(Integer id);
 
     List<DiscountData> getAllDiscounts();
-
-    List<DiscountData> getDiscountsByType(String type);
-
-    Optional<DiscountData> getDiscountById(Integer id);
-
-    Boolean validateDiscount(DiscountData discountData);
 }
