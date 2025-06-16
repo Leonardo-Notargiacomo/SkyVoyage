32233c3ef9a83277c1de7d2e4af8e84fe42e8169
Leonardo-Notargiacomo <l.notargiacomo@student.fontys.nl>
2025-06-16 14:49:02 +0200

    check that none of the values can be null

--- implementation/backend/datarecords/src/main/java/io/github/fontysvenlo/ais/datarecords/DiscountData.java
+++ implementation/backend/datarecords/src/main/java/io/github/fontysvenlo/ais/datarecords/DiscountData.java
@@ -1,4 +1,25 @@
 package io.github.fontysvenlo.ais.datarecords;
 
 public record DiscountData(Integer id, String name, Double amount, String type, Integer employeeID, Integer days) {
+
+    public DiscountData {
+        if (id == null) {
+            throw new NullPointerException("id cannot be null");
+        }
+        if (name == null) {
+            throw new NullPointerException("name cannot be null");
+        }
+        if (amount == null) {
+            throw new NullPointerException("amount cannot be null");
+        }
+        if (type == null) {
+            throw new NullPointerException("type cannot be null");
+        }
+        if (employeeID == null) {
+            throw new NullPointerException("employeeID cannot be null");
+        }
+        if (days == null) {
+            throw new NullPointerException("days cannot be null");
+        }
+    }
 }
